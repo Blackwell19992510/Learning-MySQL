139ec07b5f7694a514beece22b0cade09dd48267
muchi <65350446+Blackwell19992510@users.noreply.github.com>
2022-02-09 18:50:15 +0300

    Update game.sql

--- game.sql
+++ game.sql
@@ -1,6 +1,6 @@
 use game;
 select * from login;
-insert into login values ('black' , 'toothpick' , 19);
+insert into login values (1 , 'black' , 'toothpick' , 19);
 select * from login;
 drop table login;
 
